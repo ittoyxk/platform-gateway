8cb04dbb3399679798a697d12035936efb918b7e
xiaokang <hengxk@commchina.net>
2020-05-06 15:57:06 +0800

    重写requestBody

--- src/main/java/net/commchina/platform/gateway/filter/AuthOpenAPIGatewayFilter.java
+++ src/main/java/net/commchina/platform/gateway/filter/AuthOpenAPIGatewayFilter.java
@@ -1,31 +1,36 @@
 package net.commchina.platform.gateway.filter;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import net.commchina.platform.gateway.remote.AuthUserRemote;
 import net.commchina.platform.gateway.remote.http.req.OpenApiAuthReq;
 import net.commchina.platform.gateway.remote.http.resp.UserInfo;
 import net.commchina.platform.gateway.response.APIResponse;
+import net.commchina.platform.gateway.response.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.support.BodyInserterContext;
 import org.springframework.cloud.gateway.support.CachedBodyOutputMessage;
 import org.springframework.cloud.gateway.support.DefaultServerRequest;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -45,17 +50,59 @@ public class AuthOpenAPIGatewayFilter extends AbstractGatewayFilterFactory {
     @SuppressWarnings("unchecked")
     public GatewayFilter apply(Object config)
     {
-        return new GatewayFilter() {
-            @Override
-            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
-            {
-                String path = exchange.getRequest().getURI().getPath();
-                if (StrUtil.containsAnyIgnoreCase(path, "/openapi/")) {
+        return (exchange, chain) ->
+        {
+            String path = exchange.getRequest().getURI().getPath();
+            if (StrUtil.containsAnyIgnoreCase(path, "/openapi/")) {
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.putAll(exchange.getRequest().getHeaders());
+                // mediaType
+                MediaType mediaType = exchange.getRequest().getHeaders().getContentType();
+                HttpMethod method = exchange.getRequest().getMethod();
+                if (Objects.equals(method, HttpMethod.GET)) {
+                    String appId = exchange.getRequest().getHeaders().getFirst("appId");
+                    String timestamp = exchange.getRequest().getHeaders().getFirst("timestamp");
+                    String signType = exchange.getRequest().getHeaders().getFirst("signType");
+                    String signature = exchange.getRequest().getHeaders().getFirst("signature");
+                    String data = exchange.getRequest().getHeaders().getFirst("data");
+
+                    OpenApiAuthReq build = OpenApiAuthReq.builder().timestamp(timestamp).signType(signType).signature(signature).appId(appId).reqData(data).build();
+                    APIResponse<UserInfo> auth = authUserRemote.auth(build);
+                    if (auth.getCode() == 1) {
+                        setAuthHeader(headers, auth);
+                    } else {
+                        return ResponseEntity.errorResult(exchange.getResponse(), HttpStatus.UNAUTHORIZED, auth.getMsg());
+                    }
+                    ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                        @Override
+                        public HttpHeaders getHeaders()
+                        {
+                            long contentLength = headers.getContentLength();
+                            HttpHeaders httpHeaders = new HttpHeaders();
+                            httpHeaders.putAll(headers);
+                            if (contentLength > 0) {
+                                httpHeaders.setContentLength(contentLength);
+                            } else {
+                                httpHeaders.set(HttpHeaders.TRANSFER_ENCODING, "chunked");
+                            }
+                            return httpHeaders;
+                        }
+
+                        @Override
+                        public URI getURI()
+                        {
+                            URI uri = exchange.getRequest().getURI();
+                            URI newUri = UriComponentsBuilder.fromUri(uri)
+                                    .replaceQuery(HttpUtil.toParams(exchange.getRequest().getQueryParams()))
+                                    .build(true)
+                                    .toUri();
+                            return newUri;
+                        }
+                    };
+                    return chain.filter(exchange.mutate().request(decorator).build());
+                } else {
                     ServerRequest serverRequest = new DefaultServerRequest(exchange);
-                    HttpHeaders headers = new HttpHeaders();
-                    headers.putAll(exchange.getRequest().getHeaders());
-                    // mediaType
-                    MediaType mediaType = exchange.getRequest().getHeaders().getContentType();
                     // read & modify body
                     Mono<String> modifiedBody = serverRequest.bodyToMono(String.class)
                             .flatMap(body -> {
@@ -68,21 +115,22 @@ public class AuthOpenAPIGatewayFilter extends AbstractGatewayFilterFactory {
                                     String appId = json.getString("appId");
                                     String signature = json.getString("signature");
                                     String signType = json.getString("signType");
+
                                     OpenApiAuthReq build = OpenApiAuthReq.builder().timestamp(timestamp).signType(signType).signature(signature).appId(appId).reqData(data).build();
-                                    //log.debug("build:{}", build.toString());
                                     APIResponse<UserInfo> auth = authUserRemote.auth(build);
-                                    headers.set("companyId", auth.getData().getCompanyId() == null ? "-1" : Long.toString(auth.getData().getCompanyId()));
-                                    headers.set("enterpriseId", auth.getData().getEnterpriseId() == null ? "-1" : Long.toString(auth.getData().getEnterpriseId()));
-                                    headers.set("userId", auth.getData().getUserId() == null ? "-1" : Long.toString(auth.getData().getUserId()));
-                                    headers.set("deptId", auth.getData().getDeptId() == null ? "-1" : Long.toString(auth.getData().getDeptId()));
-                                    headers.set("groundId", auth.getData().getGroundId() == null ? "" : Long.toString(auth.getData().getGroundId()));
-                                    headers.set("requestId", UUID.randomUUID().toString());
+                                    if (auth.getCode() == 1) {
+                                        setAuthHeader(headers, auth);
+                                    } else {
+                                        headers.set("authCode", Integer.toString(auth.getCode()));
+                                        headers.set("authMsg", auth.getMsg());
+                                    }
                                     // new body map
 
                                     return Mono.just(encodeBody(data));
                                 }
                                 return Mono.empty();
                             });
+
 
                     BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
 
@@ -94,8 +142,7 @@ public class AuthOpenAPIGatewayFilter extends AbstractGatewayFilterFactory {
                     CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange, headers);
                     return bodyInserter.insert(outputMessage, new BodyInserterContext())
                             .then(Mono.defer(() -> {
-                                ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(
-                                        exchange.getRequest()) {
+                                ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
                                     @Override
                                     public HttpHeaders getHeaders()
                                     {
@@ -116,14 +163,30 @@ public class AuthOpenAPIGatewayFilter extends AbstractGatewayFilterFactory {
                                         return outputMessage.getBody();
                                     }
                                 };
-                                return chain.filter(exchange.mutate().request(decorator).build());
+                                if (Objects.equals(headers.getFirst("authCode"), "1")) {
+                                    return chain.filter(exchange.mutate().request(decorator).build());
+                                }else{
+                                    return ResponseEntity.errorResult(exchange.getResponse(), HttpStatus.UNAUTHORIZED, headers.getFirst("authMsg"));
+                                }
                             }));
-                } else {
-                    return chain.filter(exchange);
                 }
+            } else {
+                return chain.filter(exchange);
             }
         };
     }
+
+    private void setAuthHeader(HttpHeaders headers, APIResponse<UserInfo> auth)
+    {
+        headers.set("authCode", Integer.toString(auth.getCode()));
+        headers.set("companyId", auth.getData().getCompanyId() == null ? "-1" : Long.toString(auth.getData().getCompanyId()));
+        headers.set("enterpriseId", auth.getData().getEnterpriseId() == null ? "-1" : Long.toString(auth.getData().getEnterpriseId()));
+        headers.set("userId", auth.getData().getUserId() == null ? "-1" : Long.toString(auth.getData().getUserId()));
+        headers.set("deptId", auth.getData().getDeptId() == null ? "-1" : Long.toString(auth.getData().getDeptId()));
+        headers.set("groundId", auth.getData().getGroundId() == null ? "" : Long.toString(auth.getData().getGroundId()));
+        headers.set("requestId", UUID.randomUUID().toString());
+    }
+
 
     private JSONObject decodeBody(String body)
     {
