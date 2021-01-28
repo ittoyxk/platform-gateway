eb6cbc87f8fb458d6fe1923eb05a7280b0aa8c95
xiaokang <hengxk@commchina.net>
2021-01-28 10:58:39 +0800

    circuitBreaker

--- src/main/java/net/commchina/platform/gateway/PlatformGatewayApplication.java
+++ src/main/java/net/commchina/platform/gateway/PlatformGatewayApplication.java
@@ -2,10 +2,13 @@ package net.commchina.platform.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 
-@EnableFeignClients
+@EnableCircuitBreaker
+@EnableFeignClients(defaultConfiguration = FeignClientsConfiguration.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PlatformGatewayApplication {
